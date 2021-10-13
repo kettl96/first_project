ebd4e080f633c41e1dc31a583c903c34eed4df21
kettl96 <kettl110e4@gmail.com>
2021-10-14 01:22:00 +0300

    completed

--- test-proj/JS/script.js
+++ test-proj/JS/script.js
@@ -3,3 +3,10 @@ let user = "vasy"
 let userName = "vasy";
 // userName = user;
 console.log(userName);
+
+var drawCats = function (howManyTimes) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + " =^.^=");
+  }
+};
+drawCats(10);
