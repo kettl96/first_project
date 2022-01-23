6c91badaff008a34e382f0f6d172e01b712a902d
kettl96 <kettl110e4@gmail.com>
2022-01-23 16:29:18 +0300

    object test

--- test-proj/testall.js
+++ test-proj/testall.js
@@ -19,28 +19,41 @@
 
 //   setTimeout(() => circle.remove(), 500)
 // })
-const button = document.querySelector('.ripple')
-console.log(button);
+// const button = document.querySelector('.ripple')
+// console.log(button);
 
-button.addEventListener('click', function (e) {
-  const x = e.pageX
-  const y = e.pageY
-  // alert('kek')
-console.log(x,y);
-  const buttonTop = e.target.offsetTop
-  const buttonLeft = e.target.offsetLeft
-console.log(buttonTop, buttonLeft);
-  const xInside = x - buttonLeft
-  const yInside = y - buttonTop
-console.log(xInside, yInside);
-console.log(yInside);
-  const circle = document.createElement('span')
-  console.log(circle);
-  circle.classList.add('circle')
-  circle.style.top = yInside + 'px'
-  circle.style.left = xInside + 'px'
+// button.addEventListener('click', function (e) {
+//   const x = e.pageX
+//   const y = e.pageY
+//   // alert('kek')
+// console.log(x,y);
+//   const buttonTop = e.target.offsetTop
+//   const buttonLeft = e.target.offsetLeft
+// console.log(buttonTop, buttonLeft);
+//   const xInside = x - buttonLeft
+//   const yInside = y - buttonTop
+// console.log(xInside, yInside);
+// console.log(yInside);
+//   const circle = document.createElement('span')
+//   console.log(circle);
+//   circle.classList.add('circle')
+//   circle.style.top = yInside + 'px'
+//   circle.style.left = xInside + 'px'
 
-  this.appendChild(circle)
+//   this.appendChild(circle)
 
-  setTimeout(() => circle.remove(), 50000)
-})
+//   setTimeout(() => circle.remove(), 50000)
+// })
+
+
+const artists = {
+  beunce: {
+    song: 'song1',
+    name: "song1",
+    date: 'date1'
+  }
+}
+console.log('kek');
+console.log(Object.entries(artists)[0][1]);
+Object.entries(artists)
+console.log(Object.entries(artists)[0][1].song);
